4e9eb5f10250d34ff48f78dd0174597d7dbe69bb
ishikoro333 <ishikoro33_33@yahoo.co.jp>
2022-02-09 10:13:06 +0900

    AppServiceProvider fixed

--- threads/resources/views/threads/index.blade.php
+++ threads/resources/views/threads/index.blade.php
@@ -4,6 +4,43 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
+
+                {{ $threads->links() }}
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            @foreach ($threads as $thread)
+                <div class="col-md-8 mb-5">
+                    <div class="card text-center">
+                        <div class="card-header">
+                            <h3 class="m-0">{{ $thread -> name }}</h3>
+                        </div>
+                        @foreach ($thread->messages as $message)
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $loop->iteration }} 名前:{{ $message->user->name }}:{{$message->created_at }}</h5>
+                                <p class="card-text">{{ $message->body }}</p>
+                            </div>
+                        @endforeach
+                        <div class="card-footer">
+                            <form action="" method="POST" class="mb-5">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="thread-first-content">内容</label>
+                                    <textarea name="body" id="thread-first-content" class="form-control" rows="3"></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-outline-primary">書き込む</button>
+                            </form>
+                            <a href="">全部読む</a>
+                            <a href="">最新</a>
+                            <a href="">1-100</a>
+                            <a href="">リロード</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
                 @include('layouts.flash-messages')
                 <div class="card">
                     <h5 class="card-header">新規スレッド</h5>
